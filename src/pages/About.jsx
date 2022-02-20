493dc2c310878f8e8188e145f8f91b242754732f
Emas15 <thebengalifriendz@gmail.com>
2022-02-20 19:02:32 +0600

    balamar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ function About() {
       </p>
       <p className='text-lg text-gray-400'>
         Layout By:
-        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+        <a className='text-white' href=''>
           emtiaz
         </a>
       </p>
